b239ddae8bc0148b1acebf299f6e7a9e1a12bd80
Leandro Acquati <leandroacquati@gmail.com>
2020-11-11 09:09:08 -0300

    add User store

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import routes from './routes'
 
 const app = express()
 
-app.use(routes)
 app.use(express.json())
+app.use(routes)
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'))
